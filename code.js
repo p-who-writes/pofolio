d41e31ea9f02d44fb4e357e185c0220645b661ba
pbhattacharyya <31600118+pbhattacharyya@users.noreply.github.com>
2019-04-02 01:33:22 -0400

    about page

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     $(this).scrollTop(0);
 
+
     $(document).on("scroll", function () {
 
         var pageTop = $(document).scrollTop();
@@ -16,6 +17,48 @@ $(document).ready(function(){
         };
 
     });
+
+    $(function() {
+        $("body").prognroll({
+          height: 4, // progress bar height
+          color: "#FFD800", // progress bar background color
+          custom: false // if you make it true, you can add your custom div and see it's scroll progress on the page
+        });
+      });
+
+    $('a[href*="#"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
 
     $("#verizon-panel").mouseover(function() {
 

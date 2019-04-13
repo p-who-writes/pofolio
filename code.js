7d08948a12efc527d133ce3f53f08a8814fbbfc3
pbhattacharyya <31600118+pbhattacharyya@users.noreply.github.com>
2019-04-13 13:06:14 -0400

    added thesaurus

--- code.js
+++ code.js
@@ -29,6 +29,7 @@ $(document).ready(function(){
     $('a[href*="#"]')
     .click(function(event) {
       // On-page links
+      
       if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
         && 
